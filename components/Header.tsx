f819b1ecd31a93164f021f228497e6ed1f43080e
rafaelsatyro <rafael.satyro@hotmail.com>
2025-04-30 03:43:43 -0300

    feat: adicionado-header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,30 @@
+'use client'
+import Image from "next/image"
+import Logo from "../public/logo.png"
+import { useState } from "react";
+
 export default function Header() {
+    const [lang, setLang] = useState<'pt' | 'en'>('pt');
     return (
-      <header className="">
-        Header
+      <header className="flex h-20 items-center pl-8 pr-8 bg-gray-100 justify-between md:pl-10 md:pr-10 lg:pl-12 lg:pr-12 xl:pl-16 xl:pr-16 xl:h-22 2xl:pl-20 2xl:pr-20">
+        <Image 
+            src={Logo}
+            alt="Logo"
+            className="h-12 w-12 xl:w-14 xl:h-14"
+        />
+        <div className="flex items-center gap-4 md:gap-6">
+            <h1 className="text-sm text-neutral-800 xl:text-base">About</h1>
+            <h1 className="text-sm text-neutral-800 xl:text-base">Projects</h1>
+            <h1 className="text-sm text-neutral-800 xl:text-base">Contact</h1>
+            <div className="flex md:flex md:items-center md:gap-4">
+                <button
+                    onClick={() => setLang(prev => (prev === 'pt' ? 'en' : 'pt'))}
+                    className="text-sm text-neutral-800 transition xl:text-base cursor-pointer"
+                    >
+                    {lang === 'pt' ? 'EN' : 'PT-BR'}
+                </button>
+            </div>
+        </div>
       </header>
     )
   }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Nunito } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: 'Rafael Satyro - Frontend Engineer Portfolio',
   description: 'Desenvolvedor de software',
 }
 
+const nunito = Nunito({ subsets: ['latin'] })
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className={`${inter.className} bg-background text-primary`}>{children}</body>
+      <body className={`${nunito.className} bg-background text-primary`}>{children}</body>
     </html>
   )
 }

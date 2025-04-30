ab4dfedf61822110516cd0d9ee0928f26171121e
rafaelsatyro <rafael.satyro@hotmail.com>
2025-04-30 03:03:02 -0300

    feat: adicionado-componentes-header-footer-e-home-page

---
+++ components/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+      <footer className="">
+        HomePage
+      </footer>
+    )
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import HomePage from "../../components/HomePage";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center text-center px-4">
-      <h1 className="text-4xl font-bold mb-2">Olá, sou Rafael</h1>
-      <p className="text-secondary text-lg mb-8">Desenvolvedor de software e aplicações web</p>
-
-      <button className="px-6 py-2 border border-secondary text-secondary hover:bg-secondary hover:text-white transition rounded">
-        Ver meus projetos
-      </button>
-    </main>
+    <>
+      <Header/>
+      <HomePage/>
+      <Footer/>
+    </>
   )
 }

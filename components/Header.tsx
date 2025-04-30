5569243df3d8128191b78f0ae2cf09c0d367cfeb
rafaelsatyro <rafael.satyro@hotmail.com>
2025-04-30 05:01:07 -0300

    feat: inserido-home-page

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 export default function Header() {
     const [lang, setLang] = useState<'pt' | 'en'>('pt');
     return (
-      <header className="flex h-20 items-center pl-8 pr-8 bg-gray-100 justify-between md:pl-10 md:pr-10 lg:pl-12 lg:pr-12 xl:pl-16 xl:pr-16 xl:h-22 2xl:pl-20 2xl:pr-20 2xl:h-24">
+      <header className="flex h-20 items-center pl-8 pr-8 bg-gray-50 justify-between border-b border-gray-200 w-full shadow-sm border-opacity-90 md:pl-10 md:pr-10 lg:pl-12 lg:pr-12 xl:pl-16 xl:pr-16 xl:h-22 2xl:pl-20 2xl:pr-20 2xl:h-24 ">
         <div className="flex items-center gap-2">
           <>
           <Image 
@@ -14,7 +14,7 @@ export default function Header() {
             alt="Logo"
             className="h-12 w-12 xl:w-14 xl:h-14 transition ease-in-out duration-800 hover:scale-[1.05]"
           />
-           <h1 className="hidden md:flex md:text-sm md:text-neutral-800 xl:text-base md:transition md:ease-in-out md:duration-800 md:hover:scale-[1.05]">Rafael Satyro - Frontend Engineer</h1>
+           <h1 className="hidden md:flex md:text-sm md:text-neutral-800 xl:text-base md:transition md:ease-in-out md:duration-800 md:hover:scale-[1.05]">Rafael Satyro</h1>
           </>
         </div>
        
--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,7 +1,22 @@
 export default function HomePage() {
     return (
-      <footer className="">
-        HomePage
+      <footer className="flex bg-gray-50 h-screen">
+        <div className="flex items-center pl-8 pr-8 pt-8 w-full flex-col lg:pt-10">
+          <h1 className="flex text-xl font-medium pb-4 xl:text-3x1 2xl:text-3xl 2xl:pb-6 transition ease-in-out duration-800 hover:scale-[1.05] text-neutral-800">Not just a Frontend UI Engineer</h1>
+          <h1 className="flex text-lg font-light pb-8 xl:text-3x1 2xl:text-2xl transition ease-in-out duration-800 hover:scale-[1.05] text-neutral-800 2xl:pb-10">An expert in user experience development.</h1>
+            <div className="relative w-full max-w-3xl mx-auto rounded-xl overflow-hidden shadow-md">
+              <iframe
+                src="https://www.youtube.com/embed/tvTRZJ-4EyI?controls=0&modestbranding=1&rel=0&showinfo=0"
+                title="YouTube video player"
+                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                className="w-full h-[315px] sm:h-[400px]"
+              />
+            </div>
+        </div>
+        <div className="">
+        
+        </div>
       </footer>
     )
   }

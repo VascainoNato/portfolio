88d87b3e5330ca7658d6be3084ae1c6c4605e7f1
rafaelsatyro <rafael.satyro@hotmail.com>
2025-04-30 03:57:45 -0300

    fix: inserido-nome-title

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,20 +6,26 @@ import { useState } from "react";
 export default function Header() {
     const [lang, setLang] = useState<'pt' | 'en'>('pt');
     return (
-      <header className="flex h-20 items-center pl-8 pr-8 bg-gray-100 justify-between md:pl-10 md:pr-10 lg:pl-12 lg:pr-12 xl:pl-16 xl:pr-16 xl:h-22 2xl:pl-20 2xl:pr-20">
-        <Image 
+      <header className="flex h-20 items-center pl-8 pr-8 bg-gray-100 justify-between md:pl-10 md:pr-10 lg:pl-12 lg:pr-12 xl:pl-16 xl:pr-16 xl:h-22 2xl:pl-20 2xl:pr-20 2xl:h-24">
+        <div className="flex items-center gap-2">
+          <>
+          <Image 
             src={Logo}
             alt="Logo"
-            className="h-12 w-12 xl:w-14 xl:h-14"
-        />
+            className="h-12 w-12 xl:w-14 xl:h-14 transition ease-in-out duration-800 hover:scale-[1.05]"
+          />
+           <h1 className="hidden md:flex md:text-sm md:text-neutral-800 xl:text-base md:transition md:ease-in-out md:duration-800 md:hover:scale-[1.05]">Rafael Satyro - Frontend Engineer</h1>
+          </>
+        </div>
+       
         <div className="flex items-center gap-4 md:gap-6">
-            <h1 className="text-sm text-neutral-800 xl:text-base">About</h1>
-            <h1 className="text-sm text-neutral-800 xl:text-base">Projects</h1>
-            <h1 className="text-sm text-neutral-800 xl:text-base">Contact</h1>
+            <h1 className="text-sm text-neutral-800 xl:text-base transition ease-in-out duration-800 hover:scale-[1.05] cursor-pointer">About</h1>
+            <h1 className="text-sm text-neutral-800 xl:text-base transition ease-in-out duration-800 hover:scale-[1.05] cursor-pointer">Projects</h1>
+            <h1 className="text-sm text-neutral-800 xl:text-base transition ease-in-out duration-800 hover:scale-[1.05] cursor-pointer">Contact</h1>
             <div className="flex md:flex md:items-center md:gap-4">
                 <button
                     onClick={() => setLang(prev => (prev === 'pt' ? 'en' : 'pt'))}
-                    className="text-sm text-neutral-800 transition xl:text-base cursor-pointer"
+                    className="text-sm text-neutral-800 xl:text-base cursor-pointer transition ease-in-out duration-800 hover:scale-[1.05]"
                     >
                     {lang === 'pt' ? 'EN' : 'PT-BR'}
                 </button>
